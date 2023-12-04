087c0f070e51c25897fc2df28434648b6e8a99a7
niharikams <niharikams.tcs@gmail.com>
2023-12-04 07:52:32 -0700

    Sample commit

--- src/main/java/com/niharika/tutorials/controller/ProductController.java
+++ src/main/java/com/niharika/tutorials/controller/ProductController.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:3000","http://localhost:8083"})
+@CrossOrigin(origins = {"http://localhost:3002","http://localhost:8083","http://localhost:4200"})
 @RestController
 @RequestMapping("/quickbooks")
 public class ProductController {
@@ -52,9 +53,9 @@ public class ProductController {
         }
     }
 
-    @PutMapping("/products/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
-        Optional<Product> productData = productService.getProductNameById(id);
+    @PutMapping("/products")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        Optional<Product> productData = productService.getProductNameById(product.getId());
 
         if (productData.isPresent()) {
             Product productValue = productData.get();
@@ -68,7 +69,7 @@ public class ProductController {
     }
 
     @DeleteMapping("/product/{id}")
-    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable long id) {
         try {
             productService.deleteProductById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
